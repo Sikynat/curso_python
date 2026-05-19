613be4bf734031452b45442266d2ecb481d6481e
Caique Dadonas <69750914+Sikynat@users.noreply.github.com>
2026-05-18 23:16:17 -0300

    Second version to list

--- tuplas.py
+++ tuplas.py
@@ -3,7 +3,10 @@ lanche = ('Hamburguer', 'Pão', 'Tomate', 'Cebola')
 items = len(lanche)
 print(f'No meu lanche tem: {items} itens.')
 
-for comida in lanche:
-    print(f'No meu lanche tem: {comida}')
+for cont in range(0, items):
+    print(f'No meu lanche tem: {lanche[cont]}.')
+
+#for comida in lanche:
+    #print(f'No meu lanche tem: {comida}')
 
 print('Comi bastante')
